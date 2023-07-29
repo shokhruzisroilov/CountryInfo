55c4d59d79c7dfc931ae58da465332f8836c55af
shokhruzisroilov <117011473+shohcoding@users.noreply.github.com>
2023-07-29 09:26:49 +0500

    update main

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,6 @@
 import Header from './Header'
 import Search from './Search'
-import Main from './main'
+import Main from './Main'
 import Filter from './Filter'
 
 export { Header, Search, Main, Filter }
